e3a73d2421400b78cee7f34ad6d69a779d12ee5b
David Alonso <davidalonso991@gmail.com>
2024-10-28 11:40:51 -0600

    Agregado de favoritos y conexion con db y validacion

--- src/app/favorites.service.ts
+++ src/app/favorites.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  urlConfig = 'http://localhost:3000/favs';
   favs = Array<string>();
   sesion = false;
+  user = "";
 
-  constructor() {
+  constructor(private toastr: ToastrService, private http: HttpClient) {
     this.favs = [];
+    this.isAuth();
   }
 
   addFav(fav: string): void{
-    this.favs.push(fav);
+    if(this.sesion){
+      this.favs.push(fav);
+      this.toastr.success('Añadido a favoritos', 'Favoritos', { positionClass: 'toast-bottom-center' });
+      this.http.post(this.urlConfig+"/createOrUpdate", {fav, user: this.user, remove: false}).subscribe((data)=>{
+        console.log(data);
+      });
+    }else{
+      window.location.href = '/login';
+    }
   }
 
   removeFav(fav: string){
     this.favs = this.favs.filter(f => f !== fav);
+    this.toastr.info('Eliminado de favoritos', 'Favoritos', { positionClass: 'toast-bottom-center' });
   }
 
   getFavs(){
@@ -32,8 +45,12 @@ export class FavoritesService {
     let user = localStorage.getItem('user');
     if(user){
       this.sesion = true;
+      this.user = user;
     }
     this.sesion = false;
+  }
+  getFavsWithUser(){
+
   }
 
 
